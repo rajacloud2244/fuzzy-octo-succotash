773586468e153cf0e8ea78cb05b9fc9487a2470a
rajacloud2244 <spandanapaladi@outlook.com>
2024-11-04 14:55:57 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -7,13 +7,18 @@ terraform {
   }
 
   required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket         = "backendterraform2244"         # Your S3 bucket name
+    key            = "project-name/terraform.tfstate" # Path within the bucket for the state file
+    region         = "us-east-1"                     # The region where the bucket is located
+    #dynamodb_table = "terraform-lock-table"          # Optional: for state locking
+    encrypt        = true                             # Optional: enable server-side encryption
+  }
 }
 
 provider "aws" {
-  region     = "us-west-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  region     = "us-east-1"                          # Match the region of the S3 bucket
+  access_key = var.access_key                       # Ensure your access key is set correctly
+  secret_key = var.secret_key                       # Ensure your secret key is set correctly
 }
-
-
-
