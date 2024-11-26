52494c0fab23821d006175cebfc9baa7d17bb9ac
rajacloud2244 <spandanapaladi@outlook.com>
2024-11-26 18:58:24 +0530

    Update devicevolumes.tf

--- devicevolumes.tf
+++ devicevolumes.tf
@@ -1,7 +1,5 @@
-/*
 module "ebs_volumes" {
-  source                   = "./modules/volumes"  
+  source = "git::https://rajacloud2244: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@github.com/rajacloud2244/tf-aws-standard-modules.git//volumes"
   volumes  = local.volumes
   tags     = local.tags
 }
-*/
