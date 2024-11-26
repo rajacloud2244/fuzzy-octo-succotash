3a4eb68ad62d0cb4f42518e6057ec875c166c060
rajacloud2244 <spandanapaladi@outlook.com>
2024-11-26 18:57:06 +0530

    Update volume_attachment.tf

--- volume_attachment.tf
+++ volume_attachment.tf
@@ -1,9 +1,7 @@
-/*
+
 module "volume_attachment" {
-  source                   = "./modules/volume_attachment"  
+  source = "git::https://rajacloud2244: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@github.com/rajacloud2244/tf-aws-standard-modules.git//volume_attachment"
+  
   instance_ids = module.ec2_instances.instance_ids  # Get instance IDs from EC2 module
   volume_ids   = module.ebs_volumes.volume_ids       # Get volume IDs from EBS module
 }
-
-
-*/
